0e28b8f245daac27edd499344c6e8c5df8a0a2bb
dancroce <dcroce@outlook.com>
2023-07-21 21:17:50 +0100

    fix: removed image domains from next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: [process.env.NEXT_PUBLIC_SUPABASE_URL],
-  },
 }
 
 module.exports = nextConfig
